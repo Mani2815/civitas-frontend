9ee08826ff0107a363590bb0d7884522cc63aab5
mani <mani@manis-MacBook-Air.local>
2026-03-20 08:51:05 +0530

    changed

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -143,7 +143,7 @@ const DashboardLayout = () => {
                     </div>
                 </main>
             </div>
-            <ChatbotWidget />
+            {user?.role === 'citizen' && <ChatbotWidget />}
         </div>
     );
 };
